8a4039143feb66d846a4b7da0079ff301e9831f1
rodrigomarcolin <rodrigosalesmarcolin@gmail.com>
2021-06-25 21:06:21 -0300

    aprendendo

--- aprendendo/django/backend_uspapo/asgi.py
+++ aprendendo/django/backend_uspapo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for USPapo project.
+ASGI config for backend_uspapo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'USPapo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_uspapo.settings')
 
 application = get_asgi_application()
